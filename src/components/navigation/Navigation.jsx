988fea4687b33c9af276cbc0b794b5206b2dc86b
BalakaMd <md.balaka@gmail.com>
2024-10-28 22:03:25 +0200

    - bug fix

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from 'react-router-dom';
-import logo from '../../assets/logo.svg';
+import logo from '/src/assets/Logo.svg';
 import styles from './Navigation.module.css';
 
 function Navigation() {
